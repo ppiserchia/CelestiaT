8094cc9c08b5959b697abeafabc6108a31f6e242
Pasquale Piserchia <piserchia.pasquale@outlook.it>
2025-02-26 15:33:31 +0100

    Initial Commit

---
+++ C5_Stars/C5_StarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  C5_StarsApp.swift
+//  C5_Stars
+//
+//  Created by Pasquale Piserchia on 26/02/25.
+//
+
+import SwiftUI
+
+@main
+struct C5_StarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ C5_Stars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C5_Stars
+//
+//  Created by Pasquale Piserchia on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
