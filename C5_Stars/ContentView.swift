ca23342c3b2c275ddada81c21fa934a12b3334bd
Gina Saviano <gina.saviano.97@gmail.com>
2025-02-27 17:46:39 +0100

    Just checking if it works

--- C5_Stars/ContentView.swift
+++ C5_Stars/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
             Text("Hello, world!")
             Text("Hola Mario")
             Text("Hola Pasquale")
+            Text("Hola Gina")
         }
         .padding()
     }
