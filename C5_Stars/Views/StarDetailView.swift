2bd21573100f63e84e5647a5581037d5c67cb6f5
Luis Mario Recinos Hernández <92188399+Mare1702@users.noreply.github.com>
2025-03-01 13:05:15 +0100

    Create StarDetailView.swift

---
+++ C5_Stars/Views/StarDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  StarDetailView.swift
+//  C5_Stars
+//
+//  Created by Luis Mario Recinos Hernández  on 28/02/25.
+//
+
+import SwiftUI
+
+struct StarDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarDetailView()
+}
