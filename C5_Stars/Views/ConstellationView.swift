d731ff051a35197c86b4e0a72d94994d7e46246e
Gina Saviano <gina.saviano.97@gmail.com>
2025-03-03 16:26:35 +0100

    G. - commented the "path" structure so everyone is updated on it. Changed the background color and the line color

--- C5_Stars/Views/ConstellationView.swift
+++ C5_Stars/Views/ConstellationView.swift
@@ -6,6 +6,12 @@ struct ConstellationView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
+                LinearGradient(
+                    colors: [.black.opacity(0.8), .black],
+                    startPoint: .bottom,
+                    endPoint: .top)
+                .ignoresSafeArea()
+                
                 if stars.isEmpty {
                     // Generate random stars when the view appears
                     Color.clear
@@ -14,14 +20,19 @@ struct ConstellationView: View {
                         }
                 } else {
                     // Draw connecting lines
+                    ///PATH is a SwiftUI component that allows to define a custom shape by drawing lines, curves and other graphical element
+                    ///Inside the closures  `{ path in }` we define how this graphical element should be rendered
                     Path { path in
                         guard let firstPoint = stars.first else { return }
-                        path.move(to: firstPoint)
+                        /// here we are ensuring there's a first point where to draw the line: in this case, the first point is the first star of the array "stars".
+                        /// the `guard let` ensures that there is at least one point, otherwise it exits with `return`
+                        path.move(to: firstPoint) //the "drawing cursor" is moved to the first point we declared before
                         for point in stars.dropFirst() {
                             path.addLine(to: point)
                         }
+                        ///here we are connecting the remaning points: `stars.dropFirst()` is a function that gives back all the elements of the array but the first, so we can iterate on the array and connect all the stars
                     }
-                    .stroke(Color.blue, lineWidth: 3)
+                    .stroke(Color.white, lineWidth: 3)
                     
                     // Draw circles
                     ForEach(0..<stars.count, id: \.self) { index in
