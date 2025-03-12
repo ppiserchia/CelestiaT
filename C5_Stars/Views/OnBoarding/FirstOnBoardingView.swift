c3b2233848a751616d0d351f93a260ffcc4a1659
Pasquale Piserchia <piserchia.pasquale@outlook.it>
2025-03-12 17:05:03 +0100

    Created FirstOnBoardingView

---
+++ C5_Stars/Views/OnBoarding/FirstOnBoardingView.swift
@@ -0,0 +1,50 @@
+//
+//  FirstOnBoardingView.swift
+//  C5_Stars
+//
+//  Created by Pasquale Piserchia on 12/03/25.
+//
+
+import SwiftUI
+
+struct FirstOnBoardingView: View {
+    var body: some View {
+        NavigationStack {
+        ZStack {
+//            Image("T")
+//                .ignoresSafeArea()
+            RadialGradient(colors: [Color("NightSkyBlackCenter"), Color("NightSkyBlackOuter")], center: .center, startRadius: 30, endRadius: 1000)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("T")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(30)
+                
+                Text("Placeholder")
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.white)
+                
+                NavigationLink(destination: EmptyView()
+                    .navigationBarBackButtonHidden(true)) {
+                    Text("Start the experience.")
+                            .bold()
+                        .padding(15)
+                        .background(Color.white)
+                        .foregroundColor(.black)
+                        .cornerRadius(15)
+                        .clipShape(Rectangle())
+                        .padding(30)
+                }
+            }
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    FirstOnBoardingView()
+}
