5910b896d3d45f7c8a74cd8cdd81bcca9cb17b3b
Pasquale Piserchia <piserchia.pasquale@outlook.it>
2025-03-14 18:10:38 +0100

    P. uploading the new onboarding

--- C5_Stars/Views/OnBoarding/SecondOnBoardingView.swift
+++ C5_Stars/Views/OnBoarding/SecondOnBoardingView.swift
@@ -8,48 +8,65 @@ import SwiftUI
 
 struct SecondOnBoardingView: View {
     var body: some View {
-        NavigationStack {
-            
         
-        ZStack {
-            Image("T")
+        TimelineView(.animation) { timeline in
+            let x = (sin(timeline.date.timeIntervalSince1970) + 1) / 2
             
-            Rectangle()
-                .background(.thinMaterial)
-                .cornerRadius(50)
-                .frame(width: 370, height: 300)
-            
-            VStack {
-                Text("Your safe space.")
-                    .bold()
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
-                    .frame(width: 350)
-                    .multilineTextAlignment(.leading)
+            NavigationStack {
                 
-                Text("""
+                
+                ZStack {
+                    //            Image("T")
+                    
+                    MeshGradient(width: 3, height: 3, points: [
+                        [0, 0], [0.5, 0], [1, 0],
+                        [0, 0.5], [0.5, Float(x)], [1, 0.5],
+                        [0, 1], [0.5, 1], [1, 1]
+                        
+                        /// Set up the colors
+                    ], colors: [
+                        .black, .black, .black,
+                        .gray, .black, .gray,
+                        .black, .black, .black
+                    ])
+                    .ignoresSafeArea()
+                    
+                    Rectangle()
+                        .background(.thickMaterial)
+                        .cornerRadius(50)
+                        .frame(width: 370, height: 300)
+                    
+                    VStack {
+                        Text("Your safe space.")
+                            .bold()
+                            .font(.largeTitle)
+                            .foregroundColor(.black)
+                            .frame(width: 350)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text("""
 Whenever you want,
 wherever you want.
 """)
-                .font(.title)
-                .frame(width: 350, height: 70)
-                .multilineTextAlignment(.leading)
-                
-                NavigationLink(destination: ThirdOnBoardingView()
-                    .navigationBarBackButtonHidden(true)) {
-                    Text("Continue...")
-                        .bold()
-                        .padding(15)
-                        .foregroundStyle(.black)
-                        .background(.thinMaterial)
-                        .cornerRadius(15)
-                        .clipShape(Rectangle())
-                        .padding(10)
-                    
+                        .font(.title)
+                        .frame(width: 350, height: 70)
+                        .multilineTextAlignment(.leading)
+                        
+                        NavigationLink(destination: ThirdOnBoardingView()
+                            .navigationBarBackButtonHidden(true)) {
+                                Text("Continue...")
+                                    .bold()
+                                    .padding(15)
+                                    .foregroundStyle(.black)
+                                    .background(.ultraThickMaterial)
+                                    .cornerRadius(15)
+                                    .clipShape(Rectangle())
+                                    .padding(10)
+                            }
+                    }
                 }
+                
             }
-            }
-            
         }
     }
 }
--- C5_Stars/Views/OnBoarding/ThirdOnBoardingView.swift
+++ C5_Stars/Views/OnBoarding/ThirdOnBoardingView.swift
@@ -9,207 +9,109 @@ import SwiftUI
 
 struct ThirdOnBoardingView: View {
     //MARK: Declaring variables for the animations
-    @State private var firstRectangle = false
-    @State private var secondRectangle = false
-    @State private var thirdRectangle = false
-    @State private var fourthRectangle = false
+//    @State private var firstRectangle = false
+//    @State private var secondRectangle = false
+//    @State private var thirdRectangle = false
+//    @State private var fourthRectangle = false
     
+    //MARK: Declaring the property for the animation
+    @State var textCounter: Int = 0
+    @State private var onBoardingEnd: Bool = false
+
     var body: some View {
-        NavigationStack {
-            
-            //MARK: Setting up the View
-        ZStack {
-            Image("T")
-            
-            VStack {
+        GeometryReader { screen in
+            TimelineView(.animation) { timeline in
+                let x = (sin(timeline.date.timeIntervalSince1970) + 1) / 2
                 
-                // First Animation
-                if firstRectangle {
-                    Rectangle()
-                        .background(.ultraThickMaterial)
-                        .cornerRadius(50)
-                        .frame(width: 370, height: 150)
-                        .transition(.move(edge: .leading).combined(with: .opacity))
-                        .onTapGesture {
-                            withAnimation(.easeInOut(duration: 0.5)) {
-                                firstRectangle = true
+                NavigationStack {
+                    
+                    //MARK: Setting up the View
+                    ZStack {
+                        MeshGradient(width: 3, height: 3, points: [
+                            [0, 0], [0.5, 0], [1, 0],
+                            [0, 0.5], [0.5, Float(x)], [1, 0.5],
+                            [0, 1], [0.5, 1], [1, 1]
+                            
+                            /// Set up the colors
+                        ], colors: [
+                            .black, .black, .black,
+                            .gray, .black, .gray,
+                            .black, .black, .black
+                        ])
+                        .ignoresSafeArea()
+                        // -----------------
+                        
+                        //MARK: creating the animation
+                        switch textCounter {
+                        case 0:
+                            VStack {
+                                HStack {
+                                    Text("""
+Explore constellations
+""")
+                                    .foregroundStyle(.white)
+                                    .font(.system(size: 40, weight: .bold))
+                                    .multilineTextAlignment(.leading)
+                                }
+                            }
+                            
+                        case 1:
+                            HStack {
+                                Text("""
+They are made up
+of unique stories
+""")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 40, weight: .bold))
+                            }
+                            
+                        case 2:
+                            HStack {
+                                Text("""
+Just as you are.
+""")
+                            }
+                            .foregroundStyle(.white)
+                            .font(.system(size: 40, weight: .bold))
+                            .multilineTextAlignment(.leading)
+                            
+                        case 3:
+                            NavigationLink(destination: NightSkyView()
+                                .navigationBarBackButtonHidden(true)) {
+                                    Image("star_asset")
+                                        .resizable()
+                                        .frame(width: 180, height: 180)
+                                }
+                            
+                        default:
+                            Text("")
+                        }
+                    
+                    
+                    Button {
+                        withAnimation(.easeInOut(duration: 1.0)) {
+                            textCounter += 1
+                        }
+                        
+                        if textCounter == 3 {
+                            withAnimation(.bouncy(duration: 3).delay(0.5)) {
+                                onBoardingEnd.toggle()
                             }
                         }
-                    // Text in the rectangle
-                        .overlay(Text("""
-Share your own story,
-in a star.
-""")
-                            .foregroundStyle(.black)
-                            .font(.title)
-                            .multilineTextAlignment(.center)
-                        )
-                    // Amount of opacity that the rectangle has whenever it fades away
-                        .opacity(firstRectangle ? 1 : 0.5)
-                    
-                }
-                // Second Animation
-                if secondRectangle {
-                    Rectangle()
-                        .background(.ultraThickMaterial)
-                        .cornerRadius(50)
-                        .frame(width: 400, height: 160)
-                        .transition(.move(edge: .leading).combined(with: .opacity))
-                        .overlay(Text("""
-Let it belong in a constellation,
-and let that be unique,
-""")
-                            .foregroundStyle(.black)
-                            .font(.title)
-                            .multilineTextAlignment(.center)
-                        )
-                }
-                // Third Animation
-                if thirdRectangle {
-                    Rectangle()
-                        .background(.ultraThickMaterial)
-                        .cornerRadius(50)
-                        .frame(width: 380, height: 110)
-                        .transition(.move(edge: .leading).combined(with: .opacity))
-                        .overlay(Text("Just as you are.")
-                            .foregroundStyle(.black)
-                            .font(.title)
-                            .multilineTextAlignment(.center)
-                        )
-                }
-                
-                // Fourth Animation
-                if fourthRectangle {
-                    NavigationLink(destination: NightSkyView()
-                        .navigationBarBackButtonHidden(true)
-                    ) {
-                        Text("Start")
-                            .bold()
-                            .padding(15)
-                            .foregroundStyle(.black)
-                            .background(.thinMaterial)
-                            .cornerRadius(15)
-                            .clipShape(Rectangle())
-                            .padding(30)
-                            .transition(.move(edge: .leading).combined(with: .opacity))
                     }
-                }
-            }
-            
-            
-            //MARK: Enable the animation and set their duration
-            .onAppear {
-                withAnimation(.easeInOut(duration: 0.5)) {
-                    firstRectangle = true
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    withAnimation(.easeInOut(duration: 0.5)) {
-                        secondRectangle = true
+                        //MARK: create the rectangle that is clickable
+                        label: {
+                        Rectangle()
+                            .frame(maxWidth: screen.size.width, maxHeight: screen.size.height)
+                            .opacity(0)
                     }
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                    withAnimation(.easeInOut(duration: 0.5)) {
-                        thirdRectangle = true
-                    }
-                }
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                    withAnimation(.easeInOut(duration: 0.5)) {
-                        fourthRectangle = true
                     }
                 }
             }
         }
-        }
-    }
-}
+     }
+   }
 
 #Preview {
     ThirdOnBoardingView().environment(ConstellationViewModel())
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//struct ThirdOnBoardingView: View {
-//    var body: some View {
-//        ZStack {
-//            Image("T")
-//            VStack {
-//                Rectangle()
-//                    .background(.ultraThickMaterial)
-//                    .cornerRadius(50)
-//                    .frame(width: 370, height: 150)
-//                
-//                Rectangle()
-//                    .background(.ultraThickMaterial)
-//                    .cornerRadius(50)
-//                    .frame(width: 370, height: 150)
-//                
-//                Rectangle()
-//                    .background(.ultraThickMaterial)
-//                    .cornerRadius(50)
-//                    .frame(width: 370, height: 150)
-//            }
-//            VStack {
-////                Text("Your safe space.")
-////                    .bold()
-////                    .font(.largeTitle)
-////                    .foregroundColor(.black)
-////                    .frame(width: 350)
-////                    .multilineTextAlignment(.leading)
-//                
-//                Text("""
-//
-//Share your own story
-//in a star,
-//
-//let it belong in a constellation,
-//and let that constellation be unique,
-//
-//as you are.
-//""")
-//                .font(.title)
-//                .frame(width: 300, height: 390)
-//                .multilineTextAlignment(.leading)
-//                
-//                NavigationLink(destination: EmptyView()) {
-//                    Text("Continue...")
-//                        .bold()
-//                        .padding(15)
-//                        .foregroundStyle(.black)
-//                        .background(.thinMaterial)
-//                        .cornerRadius(15)
-//                        .clipShape(Rectangle())
-//                        .padding(30)
-//                    
-//                }
-//            }
-//            }
-//    }
-//}
-//
-//#Preview {
-//    ThirdOnBoardingView()
-//}
